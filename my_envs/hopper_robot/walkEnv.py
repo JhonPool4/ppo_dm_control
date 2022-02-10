3c541e041a906c4f93d4afa66ec73e560d2ed044
JhonPool4 <jhoncharaja@gmail.com>
2022-02-10 09:43:24 -0300

    new reward system

--- my_envs/hopper_robot/walkEnv.py
+++ my_envs/hopper_robot/walkEnv.py
@@ -8,12 +8,14 @@ import os
 
 # reward parameters
 _STANDUP_LOWER_TRESHOLD = 1.15 # meters
-_STANDUP_UPPER_TRESHOLD = 2.0 # meters
-_MAX_STANDUP_ERROR = 0.5 # meters
+_STANDUP_UPPER_TRESHOLD = 2.5 # meters
+_MAX_STANDUP_ERROR = 1 # meters
+
+_MAX_XCOM_DISTANCE= 0.2 #meters
 
 _POSTURAL_LOWER_TRESHOLD = np.deg2rad(-30) # degrees
 _POSTURAL_UPPER_TRESHOLD = np.deg2rad(30) # degrees
-_MAX_POSTURAL_ERROR = np.deg2rad(20) # degrees
+_MAX_POSTURAL_ERROR = np.deg2rad(10) # degrees
 
 _VELOCITY_LOWER_TRESHOLD = 1.3 # meters/seconds
 _VELOCITY_UPPER_TRESHOLD = 1.5 # meters/seconds
@@ -29,6 +31,13 @@ _OBSERVATION_DIMENSION = 19
 #path of rl environemnt
 _RL_DIR='/home/jhon/reinforcement_learning/ppo_dm_control/my_envs/hopper_robot'
 _MODEL_FILENAME='hopper.xml'    
+
+
+
+# color
+_MATERIALS = "body"
+_DEFAULT = "body_default"
+_HIGHLIGHT = "body_highlight"
 
 
 def get_model(env_path, model_filename):
@@ -57,12 +66,14 @@ class Walk(base.Task):
         super().__init__(random=random)
     
     def initialize_episode(self, physics):
+        
         physics.named.data.qpos['root_xtras']=0.0
-        physics.named.data.qpos['root_yrot']=np.deg2rad(-90)+ np.random.uniform(-_ANGLE_DISTURBANCE, _ANGLE_DISTURBANCE)
+        physics.named.data.qpos['root_yrot']=np.deg2rad(0)+ np.random.uniform(-_ANGLE_DISTURBANCE, _ANGLE_DISTURBANCE)
         physics.named.data.qpos['root_ztras']=0.0
-        physics.named.data.qpos['hip']= np.deg2rad(-90) + np.random.uniform(-_ANGLE_DISTURBANCE, _ANGLE_DISTURBANCE)
-        physics.named.data.qpos['knee']= np.deg2rad(50) + np.random.uniform(-_ANGLE_DISTURBANCE, _ANGLE_DISTURBANCE)
-        physics.named.data.qpos['ankle']= np.deg2rad(30) + np.random.uniform(-_ANGLE_DISTURBANCE, _ANGLE_DISTURBANCE)
+        physics.named.data.qpos['waist'] = np.deg2rad(0)+np.random.uniform(-_ANGLE_DISTURBANCE, _ANGLE_DISTURBANCE)
+        physics.named.data.qpos['hip']= np.deg2rad(-100) + np.random.uniform(-_ANGLE_DISTURBANCE, _ANGLE_DISTURBANCE)
+        physics.named.data.qpos['knee']= np.deg2rad(80) + np.random.uniform(-_ANGLE_DISTURBANCE, _ANGLE_DISTURBANCE)
+        physics.named.data.qpos['ankle']= np.deg2rad(0) + np.random.uniform(-_ANGLE_DISTURBANCE, _ANGLE_DISTURBANCE)
         
         super().initialize_episode(physics)
 
@@ -87,20 +98,27 @@ class Walk(base.Task):
         # trunk: angle and  zcom
         trunk_angle = obs[6]
         trunk_zcom = obs[7]    
-
+        # foot COM
+        foot_xcom = obs[14]
         # body COM: velocity
         body_dxcom = obs[3]
+        # body COM
+        body_xcom = obs[0]
+        body_xcom_max = foot_xcom + 0.11
+        body_xcom_min = foot_xcom - 0.11        
+        
         # reward system: to encourage stand up
-        stand_reward = rewards.tolerance(trunk_zcom,
+        standup_reward = rewards.tolerance(trunk_zcom,
                                             bounds=(_STANDUP_LOWER_TRESHOLD, _STANDUP_UPPER_TRESHOLD),
                                             margin=_MAX_STANDUP_ERROR/2,
                                             value_at_margin=0.5,
-                                            sigmoid='gaussian') 
+                                            sigmoid='linear') 
         
+        """
         standup_reward = rewards._sigmoids((_STANDUP_UPPER_TRESHOLD-trunk_zcom)/_STANDUP_UPPER_TRESHOLD,
                                             value_at_1=1e-10,
                                             sigmoid='linear')  
-
+        """
 
         # reward system: to encourage forward motion and velocity tracking
         forward_reward = rewards.tolerance( body_dxcom, 
@@ -109,6 +127,8 @@ class Walk(base.Task):
                                             value_at_margin=0.5,
                                             sigmoid='linear')
         
+
+        """
         # reward system: to encourage good posture      
         posture_reward = rewards.tolerance(trunk_angle,
                                             bounds=(_POSTURAL_LOWER_TRESHOLD, _POSTURAL_UPPER_TRESHOLD),
@@ -116,8 +136,22 @@ class Walk(base.Task):
                                             value_at_margin=0.5,
                                             sigmoid='gaussian')
 
+        # reward system: stability encourage
+        
+        if np.logical_and(obs[17]>0, obs[18]>0):
+            stability_reward = rewards.tolerance(body_xcom,
+                                                bounds=(body_xcom_min, body_xcom_max),
+                                                margin=_MAX_XCOM_DISTANCE/2,
+                                                value_at_margin=0.5,
+                                                sigmoid='gaussian')
+                                                        
+        else:
+            stability_reward=0    
+        """                                                    
+
         # final reward system: standup * forward * postural       
-        return (1 + 3*stand_reward + 0.5*standup_reward)*(1 + 5*forward_reward)*(1 + 0.1*posture_reward)        
+        cost=(1+5*standup_reward)*(1+3*forward_reward) #*(1+0.1*stability_reward)*(1+0.05*posture_reward)        
+        return cost/(6*4)
 
 class CustomEnv():
     def __init__(self,
@@ -153,20 +187,31 @@ class CustomEnv():
         # apply action
         self._task.before_step(action, self._physics)
         self._physics.step()
-        self._task.after_step(self._physics)      
-        
         # observation of initial states
         observation=self._task.get_observation(self._physics)
         # reward
-        reward = self._task.get_reward(observation)          
+        reward = self._task.get_reward(observation)
+        # colors
+        self._set_reward_colors(self._physics, reward)          
         # steps
         self._step_count += 1
 
         if self._step_count>=self._step_limit: # end episode: max steps
             return observation, 0.0, True, {}
         
+        #if abs(observation[6])>=abs(np.deg2rad(135)):
+        #    return observation, 0.0, True, {}
         else:
             return observation, reward, False, {} # learning   
+
+    def _set_reward_colors(self, physics, reward):
+        assert 0.0 <= reward <= 1.0
+        colors = physics.named.model.mat_rgba
+        default = colors[_DEFAULT]
+        highlight = colors[_HIGHLIGHT]
+        blend_coef = reward ** 4  # Better color distinction near high rewards.
+        colors[_MATERIALS] = blend_coef * highlight + (1.0 - blend_coef) * default
+
         
 
 
